04b0979214e49cfce6ce9f3b8b8a9a68ba149ed7
SuleymanShah <snalbant3399@gmail.com>
2022-08-02 17:19:47 +0100

    constractor directory is added

--- src/accessModifierReview/practiceTask.java
+++ src/accessModifierReview/practiceTask.java
@@ -6,7 +6,7 @@ public class practiceTask {
 
         System.out.println("Hello this my fist GitHub sharing project!");
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 2; i++) {
 
             System.out.println("It is Cool! Well done....");
 
