c103bcb7f50cc037cea3b48edaa9b79d04069d78
SuleymanShah <snalbant3399@gmail.com>
2022-08-02 17:01:33 +0100

    This first project that I am sharing!

---
+++ src/accessModifierReview/practiceTask.java
@@ -0,0 +1,18 @@
+package accessModifierReview;
+
+public class practiceTask {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello this my fist GitHub sharing project!");
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("It is Cool! Well done....");
+
+        }
+
+
+    }
+
+}
